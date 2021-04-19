7eb1721a353148bb8bacc3db07ac5285d70f9ec7
gera98k <apariciog126@gmail.com>
2021-04-19 01:54:50 -0500

    CRUD trabajadores

---
+++ protected/controller/ControllerEditartrabajador.php
@@ -0,0 +1,19 @@
+<?php
+class controllerEditartrabajador extends Controller {
+    function __construct($view, $conf, $var, $acc) {
+        parent::__construct($view, $conf, $var, $acc);
+    } 
+    public function main() {
+        //var_dump($this->var );
+        foreach ($this->var as $key => $value) {
+            $this->data[$key]=$value;
+            $$key = $value;
+        }
+        $dominio = "profesor";
+        $this->data["datos"] = (object) indexModel::bd($this->conf)->getSQL("SELECT * FROM trabajador WHERE id = {$idReg}")[0];
+        $this->data["accion"] = "Editar";
+        $this->data["dominio"] = "trabajador";
+        $this->view->show("edittrabajador.twig", $this->data, $this->accion); 
+    }
+}
+?>
---
+++ protected/controller/ControllerTrabajador.php
@@ -0,0 +1,56 @@
+<?php
+class ControllerTrabajador extends Controller
+{
+    function __construct($view, $conf, $var, $acc)
+    {
+        parent::__construct($view, $conf, $var, $acc);
+    }
+    public function main()
+    {
+        foreach ($this->var as $key => $value){
+            
+            $$key = $value;
+        }
+        $dominio = "trabajador"; 
+        // --> Agregar registro 
+        if(isset($cmdGuardar)){
+            //$sql="INSERT INTO rol (id,rol) VALUE (0, '{$txtRol}')";
+            //indexModel::bd($this->conf)->getSQL($sql);
+            $arr= array(
+                "Dominio"=>"trabajador",
+                "txttrabajador"=>$txttrabajador,
+                "txttelefono"=>$txttelefono
+            );
+            indexModel::bd($this->conf)->updateDominio($arr);
+        }
+
+        // --> Para borrar registro
+        if($Action=="delete"){
+            //$sql="DELETE FROM {$Dominio} WHERE id = ".$idReg;
+            //indexModel::bd($this->conf)->getSQL($sql);
+            indexModel::bd($this->conf)->deleteDominio("trabajador",$idReg);
+        }
+
+        // --> Editar registro 
+        if(isset($cmdEditar)){
+            //$sql="UPDATE rol SET rol = '$txtRol' WHERE id = ".$idReg;
+            //indexModel::bd($this->conf)->getSQL($sql);
+            $arr= array(
+                "Dominio"=>"trabajador",
+                "txttrabajador"=>$txttrabajador,
+                "txttelefono"=>$txttelefono
+            );
+            indexModel::bd($this->conf)->updateDominio($arr,$idReg);
+        }
+
+        $this->data["dominio"] = $dominio ;
+        // --> Extraer datos
+        $this->data["datos"] = indexModel::bd($this->conf)->getDominio($dominio);
+        // --> Extraer datos
+        $this->data["datos"] = indexModel::bd($this->conf)->getSQL("SELECT * FROM trabajador");
+
+        asort($this->data["datos"]);
+        $this->view->show("trabajador.twig", $this->data, $this->accion);
+    }
+
+}
